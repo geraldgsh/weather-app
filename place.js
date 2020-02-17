1bacdbd633a37a9e613b9fe6c6bbd7fd82cb1062
geraldgsh <geraldgsh@hotmail.com>
2020-02-17 21:56:23 +0800

    added weather card

---
+++ place.js
@@ -0,0 +1,85 @@
+{
+  "html_attributions" : [],
+  "results" : [
+     {
+        "formatted_address" : "Times Square Shopping Centre, Bandar Seri Begawan BB2713, Brunei",
+        "geometry" : {
+           "location" : {
+              "lat" : 4.9393036,
+              "lng" : 114.9393672
+           },
+           "viewport" : {
+              "northeast" : {
+                 "lat" : 4.940559629892721,
+                 "lng" : 114.9407346798927
+              },
+              "southwest" : {
+                 "lat" : 4.937859970107278,
+                 "lng" : 114.9380350201073
+              }
+           }
+        },
+        "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+        "id" : "032025fd842e25f906c33e4708178390800d6bd4",
+        "name" : "Casbah London Kebab Time Square",
+        "opening_hours" : {
+           "open_now" : false
+        },
+        "photos" : [
+           {
+              "height" : 2988,
+              "html_attributions" : [
+                 "\u003ca href=\"https://maps.google.com/maps/contrib/102486108910849542085\"\u003eJamil Hamid\u003c/a\u003e"
+              ],
+              "photo_reference" : "CmRaAAAACIY3MuyrE5azXib5uvxdjkDA_dkjtRO6TLYzc2_7m9zNB-HrvuQoZMPhDAqF9l6kMaeX7BQzMUpUczRg1OA1h_y3l0ZfbhpyAEiFGePDogHzCCMg-L6y2nuHCseSPTLjEhDtu59MRv6Gj3BJ2241do4eGhSwodJsh2LC6fOduspY1sh1840A_Q",
+              "width" : 5312
+           }
+        ],
+        "place_id" : "ChIJwc5He6X1IjIRUQJxOsnFnb4",
+        "plus_code" : {
+           "compound_code" : "WWQQ+PP Bandar Seri Begawan",
+           "global_code" : "6PPPWWQQ+PP"
+        },
+        "rating" : 3.3,
+        "reference" : "ChIJwc5He6X1IjIRUQJxOsnFnb4",
+        "types" : [ "restaurant", "food", "point_of_interest", "establishment" ],
+        "user_ratings_total" : 4
+     },
+     {
+        "formatted_address" : "London, UK",
+        "geometry" : {
+           "location" : {
+              "lat" : 51.5073509,
+              "lng" : -0.1277583
+           },
+           "viewport" : {
+              "northeast" : {
+                 "lat" : 51.6723432,
+                 "lng" : 0.148271
+              },
+              "southwest" : {
+                 "lat" : 51.38494009999999,
+                 "lng" : -0.3514683
+              }
+           }
+        },
+        "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png",
+        "id" : "b1a8b96daab5065cf4a08f953e577c34cdf769c0",
+        "name" : "London",
+        "photos" : [
+           {
+              "height" : 1920,
+              "html_attributions" : [
+                 "\u003ca href=\"https://maps.google.com/maps/contrib/116570538887865527912\"\u003eA Google User\u003c/a\u003e"
+              ],
+              "photo_reference" : "CmRaAAAARw2eLmERJSosvs_21fnahZ6Hqqc8VsywsQH5lWzEKX6avjS5JeY6QQJlRsOSLpaonsNSkdt7yx_j9smZRcFxSEU4Qz056Pyu5_-Yh2w7hLuTF2-SjtuFg8JnSAdDc6vOEhAoR4kM3319Cmjh7msLj0lIGhRlt8_1L975bTaj3Udunq69VcF1zQ",
+              "width" : 2160
+           }
+        ],
+        "place_id" : "ChIJdd4hrwug2EcRmSrV3Vo6llI",
+        "reference" : "ChIJdd4hrwug2EcRmSrV3Vo6llI",
+        "types" : [ "locality", "political" ]
+     }
+  ],
+  "status" : "OK"
+}
